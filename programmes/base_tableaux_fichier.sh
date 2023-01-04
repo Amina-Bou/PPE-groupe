bfe709a6ac9109115cbf234fffdd40eaf3e92e8a
Amina-Bou <114527347+Amina-Bou@users.noreply.github.com>
2023-01-04 09:59:59 +0100

    Update base_tableaux_fichier.sh

--- programmes/base_tableaux_fichier.sh
+++ programmes/base_tableaux_fichier.sh
@@ -20,7 +20,7 @@ basename=$(basename -s .txt $fichier_urls)
 echo "<html><head><link rel="stylesheet" href="style.css"></head><body>" > $fichier_tableau
 echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
 echo "<br/>" >> $fichier_tableau
-echo "<table>" >> $fichier_tableau
+echo "<table class="greyGridTable">" >> $fichier_tableau
 echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th><th>dump_html</th><th>dump_text</th><th>occurrences</th><th>contextes</th><th>concordances</th></tr>" >> $fichier_tableau
 
 lineno=1;
